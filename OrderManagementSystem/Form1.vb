8d225d390b6cacb80b0cfe0960201f3babaea87b
Temot69 <Ferrertemot@gmail.com>
2025-09-19 10:04:47 +0800

    commit

--- OrderManagementSystem/Form1.vb
+++ OrderManagementSystem/Form1.vb
@@ -44,7 +44,6 @@
 
         MessageBox.Show("Your Total is: " & finalTotal.ToString & "", "Thank you " & txtName.Text & "!")
 
-
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
